8c62c788471e47ee10c67950a4509bfb3454532d
Juliano Custodio <juliano.custodio@hotmail.com.br>
2018-06-06 20:42:08 -0300

    Implement Popup

--- DemoPopup/DemoPopup/DemoPopup/MainPage.xaml.cs
+++ DemoPopup/DemoPopup/DemoPopup/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,10 @@ namespace DemoPopup
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void OpenPopup(object sender, EventArgs e)
+        {
+            PopupNavigation.PushAsync(new PopupView());
+        }
+    }
 }
